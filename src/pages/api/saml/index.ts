8a90f6fe0f7e2c24ac75122352316307044abf4a
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-06 11:03:27 -0600

    6Nov2023

---
+++ src/pages/api/saml/index.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+type ResponseData = {
+  samlToken?: string;
+  message?: string;
+};
+
+var rand = function () {
+  return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+var token = function () {
+  return rand() + rand(); // to make it longer
+};
+
+const cors = Cors({
+  methods: ["POST"],
+  origin: ["https://parent-rev-proxy.vercel.app"],
+  credentials: true,
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function,
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
+  if (req.method !== "POST")
+    return res.status(405).json({ message: "Method not allowed" });
+  runMiddleware(req, res, cors);
+  res.status(200).json({ samlToken: token() });
+}
